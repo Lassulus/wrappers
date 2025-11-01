a5f0a78e21ec27755d986a72b046f46ca2d85230
David Wolff <david@dav.dev>
2025-11-01 23:45:45 +0100

    modules/jujutsu: init

---
+++ modules/jujutsu/module.nix
@@ -0,0 +1,33 @@
+{
+  config,
+  lib,
+  ...
+}:
+
+let
+  tomlFmt = config.pkgs.formats.toml { };
+in
+{
+  options = {
+    settings = lib.mkOption {
+      type = tomlFmt.type;
+      description = ''
+        Configuration for jujutsu.
+        See <https://jj-vcs.github.io/jj/latest/config/>
+      '';
+    };
+  };
+
+  config = {
+    package = lib.mkDefault config.pkgs.jujutsu;
+    env = {
+      JJ_CONFIG = builtins.toString (tomlFmt.generate "jujutsu.toml" config.settings);
+    };
+
+    meta = {
+      maintainers = [
+        lib.maintainers.dav-wolff
+      ];
+    };
+  };
+}
