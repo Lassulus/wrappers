053a1c91f74592a4352d800bc8ea444334c7e48a
turbio <git@turb.io>
2025-11-15 00:20:35 +0700

    new modules

--- modules/niri/module.nix
+++ modules/niri/module.nix
@@ -1,49 +1,47 @@
 {
+  config,
   wlib,
   lib,
   ...
 }:
-wlib.wrapModule (
-  { config, wlib, ... }:
-  {
-    options = {
-      "config.kdl" = lib.mkOption {
-        type = wlib.types.file config.pkgs;
-        default.content = "";
-        description = ''
-          Configuration file for Niri.
-          See <https://github.com/YaLTeR/niri/wiki/Configuration:-Introduction>
-        '';
-        example = ''
-          input {
-            keyboard {
-                numlock
-            }
-
-            touchpad {
-                tap
-                natural-scroll
-            }
+{
+  _class = "wrapper";
+  options = {
+    "config.kdl" = lib.mkOption {
+      type = wlib.types.file config.pkgs;
+      default.content = "";
+      description = ''
+        Configuration file for Niri.
+        See <https://github.com/YaLTeR/niri/wiki/Configuration:-Introduction>
+      '';
+      example = ''
+        input {
+          keyboard {
+              numlock
           }
-        '';
-      };
-    };
 
-    config.filesToPatch = [
-      "share/applications/*.desktop"
-      "share/systemd/user/niri.service"
-    ];
-    config.package = lib.mkDefault config.pkgs.niri;
-    config.env = {
-      NIRI_CONFIG = builtins.toString config."config.kdl".path;
+          touchpad {
+              tap
+              natural-scroll
+          }
+        }
+      '';
     };
-    config.meta.maintainers = [
-      {
-        name = "turbio";
-        github = "turbio";
-        githubId = 1428207;
-      }
-    ];
-    config.meta.platforms = lib.platforms.linux;
-  }
-)
+  };
+  config.filesToPatch = [
+    "share/applications/*.desktop"
+    "share/systemd/user/niri.service"
+  ];
+  config.package = lib.mkDefault config.pkgs.niri;
+  config.env = {
+    NIRI_CONFIG = builtins.toString config."config.kdl".path;
+  };
+  config.meta.maintainers = [
+    {
+      name = "turbio";
+      github = "turbio";
+      githubId = 1428207;
+    }
+  ];
+  config.meta.platforms = lib.platforms.linux;
+}
