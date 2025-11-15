eaa4ea434b34fd0a78dd56e89b5871623b8a9ecb
turbio <git@turb.io>
2025-11-15 13:56:06 +0700

    add check for niri check validation failure

--- modules/niri/check.nix
+++ modules/niri/check.nix
@@ -11,9 +11,19 @@ let
       "config.kdl".content = "";
     }).wrapper;
 
+  niriWrappedBadConfig =
+    (self.wrapperModules.niri.apply {
+      inherit pkgs;
+
+      "config.kdl".content = "
+        invalid_kdl_config
+      ";
+    }).wrapper;
+
 in
 pkgs.runCommand "niri-test" { } ''
   "${niriWrapped}/bin/niri" --version | grep -q "${niriWrapped.version}"
   "${niriWrapped}/bin/niri" validate
+  ! "${niriWrappedBadConfig}/bin/niri" validate || exit 1
   touch $out
 ''
