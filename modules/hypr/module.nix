92dc65655b0364d1f7ffb277bf8024775283c154
PeDro0210 <pedroruben021@gmail.com>
2025-11-25 11:36:07 -0600

    modules/hypr: adding first setup

---
+++ modules/hypr/module.nix
@@ -0,0 +1,35 @@
+{
+  config,
+  lib,
+  wlib,
+  ...
+}:
+let
+  hypr_config = "hypr.conf";
+in
+{
+  _class = "wrapper";
+  options = {
+    ${hypr_config} = lib.mkOption {
+      type = wlib.types.file config.pkgs;
+      default = { };
+      description = ''
+      '';
+    };
+  };
+
+  config.flags = {
+    "--config" = config.${hypr_config}.path;
+  };
+
+  config.package = config.pkgs.hyprland;
+
+  config.meta.maintainers = [
+    {
+      name = "PeDro0210";
+      github = "PeDro0210";
+      githubId = 123851480;
+    }
+  ];
+  config.meta.platforms = lib.platforms.linux;
+}
