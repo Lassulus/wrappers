a0bdfdf80889691b4b25edacbffac3862084adb9
Mrid22 <mridulaga@outlook.com>
2026-01-14 11:21:05 +0100

    zsh module: add history options

---
+++ modules/zsh/module.nix
@@ -0,0 +1,73 @@
+{
+  config,
+  lib,
+  wlib,
+  ...
+}: {
+  _class = "wrapper";
+  options = {
+    settings = {
+      keyMap = lib.mkOption {
+        type = lib.types.enum ["vim" "emacs"];
+        description = ''
+          keymap for zsh, pick between emacs and vi mode, defaults to emacs mode ( will add viins and vicmd options eventually).
+        '';
+        default = "emacs";
+      };
+      history = {
+        saveNoDups = lib.mkOption {
+          type = lib.types.bool;
+          description = ''
+            doesnt save a new line if you type in a command already in the history, defaults to false.
+          '';
+          default = false;
+        };
+        expireDuplicatesFirst = lib.mkOption {
+          type = lib.types.bool;
+        };
+        append = lib.mkOption {
+          type = lib.types.bool;
+          description = ''
+            history from new zsh sessions will be appended to the hist file instead of replacing the old session history, defaults to false.
+          '';
+          default = false;
+        };
+        findNoDups = lib.mkOption {
+          type = lib.types.bool;
+          description = ''
+            ignore duplicate history when doing a reverse search.
+          '';
+          default = false;
+        };
+        ignoreDups = lib.mkOption {
+          type = lib.types.bool;
+          description = ''
+            sets the --histignoredups flag. TODO: figure out difference with saveNoDups for better docs.
+          '';
+          default = false;
+        };
+      };
+    };
+  };
+  config = {
+    flagSeparator = "=";
+    flags = {
+      "--vim" = lib.mkIf (config.keyMap == "vim");
+      "--emacs" = lib.mkIf (config.keyMap == "emacs");
+      "--histsavenodups" = lib.mkIf config.history.saveNoDups;
+      "--histexpiredupsfirst" = lib.mkIf config.history.expireDuplicatesFirst;
+      "--histappend" = lib.mkIf config.history.append;
+      "--histfindnodups" = lib.mkIf config.history.findNoDups;
+    };
+
+    package = config.pkgs.zsh;
+
+    meta.maintainers = [
+      {
+        name = "mrid22";
+        github = config.meta.maintainers.name;
+        githubId = 82091823;
+      }
+    ];
+  };
+}
