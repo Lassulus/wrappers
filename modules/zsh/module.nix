34a0dacd8872636009231d5f3bb7b8c25d6817c3
Mrid22 <mridulaga@outlook.com>
2026-01-15 14:20:59 +0100

    init shellAliases (?)

---
+++ modules/zsh/module.nix
@@ -0,0 +1,43 @@
+{
+  wlib,
+  lib,
+  ...
+}:
+wlib.wrapModule ({
+  config,
+  lib,
+  ...
+}: let
+  aliasesstr = lib.concatMapAttrsStringSep "/n" (k: v: "alias -- ${k}=${v}") config.settings.shellAliases;
+in {
+  options = {
+    settings = {
+      history = {};
+      shellAliases = lib.mkOption {
+        type = with lib.types; attrsOf str;
+        default = {};
+        descritpiton = ''
+          shell Aliases
+        '';
+      };
+    };
+  };
+  config = let
+    zshConfigDir = config.pkgs.linkFarmFromDrvs "zsh-config-directory" [
+      aliasesstr
+    ];
+  in {
+    package = config.pkgs.zsh;
+    env.Z_DOT_DIR = "${zshConfigDir}";
+    meta = {
+      maintainers = [
+        {
+          name = "mrid22";
+          github = "mrid22";
+          githubId = 80290157;
+        }
+      ];
+      platforms = lib.platforms.all;
+    };
+  };
+})
