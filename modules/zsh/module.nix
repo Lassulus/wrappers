ffada8531a01bb79be4f5885e1760d54bac41409
Mrid22 <mridulaga@outlook.com>
2026-01-15 12:41:03 +0100

    reworking it to fit the z_dot_dir var

--- modules/zsh/module.nix
+++ modules/zsh/module.nix
@@ -1,144 +1,59 @@
 {
-  config,
-  lib,
   wlib,
+  lib,
   ...
-}: let
-  cfg = config.settings;
-in {
-  _class = "wrapper";
-  options = {
-    settings = {
-      keyMap = lib.mkOption {
-        type = lib.types.enum [
-          "viins"
-          "emacs"
-          "vicmd"
-        ];
-        description = ''
-          keymap for zsh, pick between emacs vi, and vicmd, defaults to emacs mode.
-        '';
-        default = "emacs";
-      };
-
-      autocd = lib.mkOption {
-        type = lib.types.bool;
-        description = ''
-          lets you navigate to a directory just by typing the name/path. defaults to false;
-        '';
-        default = false;
-      };
-
-      completion.enable = lib.mkOption {
-        type = lib.types.bool;
-        description = ''
-          enable completion, false by default.
-        '';
-        default = false;
-      };
-
-      shellAliases = lib.mkOption {
-        type = with lib.types; attrsOf str;
-        description = ''
-          aliases
-        '';
-        default = {};
-      };
-
-      history = {
-        saveNoDups = lib.mkOption {
-          type = lib.types.bool;
-          description = ''
-            doesn't save a new line if you type in a command already in the history, defaults to false.
-          '';
-          default = false;
-        };
-        expireDuplicatesFirst = lib.mkOption {
-          type = lib.types.bool;
-          description = ''
-            expire duplicates first.
-          '';
-          default = false;
-        };
-        append = lib.mkOption {
-          type = lib.types.bool;
-          description = ''
-            history from new zsh sessions will be appended to the hist file instead of replacing the old session history, defaults to false.
-          '';
-          default = false;
-        };
-        findNoDups = lib.mkOption {
-          type = lib.types.bool;
-          description = ''
-            ignore duplicate history when doing a reverse search.
-          '';
-          default = false;
-        };
-        ignoreDups = lib.mkOption {
-          type = lib.types.bool;
-          description = ''
-            sets the --histignoredups flag. TODO: figure out difference with saveNoDups for better docs.
-          '';
-          default = false;
-        };
-        ignoreSpace = lib.mkOption {
-          description = ''
-            lets you omit a command from the history if you put a space before it; defaults to true;
-          '';
-          default = true;
-        };
-      };
-      ".zshrc" = lib.mkOption {
-        type = wlib.types.file config.pkgs;
-        default = let
-          aliasStr = builtins.concatStringsSep "\n" (
-            lib.mapAttrsToList (k: v: "alias -- ${lib.escapeShellArg k}=${lib.escapeShellArg v}")
-            cfg.shellAliases
-          );
-        in {
-          path = builtins.concatStringsSep "\n" [
-            (
-              if cfg.keyMap == "vicmd"
-              then "bindkey -a"
-              else if cfg.keyMap == "viins"
-              then "bindkey -v"
-              else "bindkey -e"
-            )
-            aliasStr
-          ];
-        };
-      };
-    };
-  };
-  config = let
-    zshConfigDir = config.pkgs.linkFarmFromDrvs "zsh-config-directory" [
-      cfg.".zshrc"
-    ];
+}:
+wlib.wrapModule (
+  {config, ...}: let
+    cfg = config.settings;
   in {
-    flagSeparator = "=";
-    flags = {
-      "--histsavenodups" = cfg.history.saveNoDups;
-      "--histexpiredupsfirst" = cfg.history.expireDuplicatesFirst;
-      "--histappend" = cfg.history.append;
-      "--histfindnodups" = cfg.history.findNoDups;
-      "--histignoredups" = cfg.history.ignoreDups;
-      "--histignorespace" = cfg.history.ignoreSpace;
-      "--autocd" = cfg.autocd;
-      "--autolist" = cfg.completion.enable;
-      "--automenu" = cfg.completion.enable;
-    };
-    env = {
-      Z_DOT_DIR = "${zshConfigDir}";
+    options = {
+      settings = {
+        keyMap = lib.mkOption {
+          type = lib.types.enum [
+            "viins"
+            "emacs"
+            "vicmd"
+          ];
+          description = ''
+            keymap for zsh, pick between emacs vi, and vicmd, defaults to emacs mode.
+          '';
+          default = "emacs";
+        };
+        ".zshrc" = lib.mkOption {
+          type = wlib.types.file config.pkgs;
+          default = let
+            aliasStr = builtins.concatStringsSep "\n" (
+              lib.mapAttrsToList (k: v: "alias -- ${lib.escapeShellArg k}=${lib.escapeShellArg v}")
+              cfg.shellAliases
+            );
+          in {
+            path = builtins.concatStringsSep "\n" [
+              (
+                if cfg.keyMap == "vicmd"
+                then "bindkey -a"
+                else if cfg.keyMap == "viins"
+                then "bindkey -v"
+                else "bindkey -e"
+              )
+              aliasStr
+            ];
+          };
+        };
+      };
     };
 
-    package = config.pkgs.zsh;
+    config = let
+      RC = config.settings.".zshrc";
+      ENV = config.settings.".zshenv";
 
-    meta.maintainers = [
-      {
-        name = "mrid22";
-        github = config.meta.maintainers.name;
-        githubId = 82091823;
-      }
-    ];
-  };
-}
+      zshConfigDir = config.pkgs.linkFarmFromDrvs "zsh-config-directory" [
+        RC
+        ENV
+      ];
+    in {
+      package = config.pkgs.zsh;
+      env.Z_DOT_DIR = "${zshConfigDir}";
+    };
+  }
+)
