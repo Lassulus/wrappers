0a5acebe9ebf1de4941febf486e16f1a4f18b6e0
Mrid22 <mridulaga@outlook.com>
2026-01-15 10:12:30 +0100

    fixed option reference

--- modules/zsh/module.nix
+++ modules/zsh/module.nix
@@ -124,7 +124,7 @@ in {
       "--automenu" = cfg.completion.enable;
     };
     env = {
-      Z_DOT_DIR = builtins.dirOf toString (".zshrc".path);
+      Z_DOT_DIR = builtins.dirOf cfg.".zshrc".path;
     };
 
     package = config.pkgs.zsh;
