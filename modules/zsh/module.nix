eeafeb933927f45fe816ecaa2ca3879bc8e47973
Mrid22 <mridulaga@outlook.com>
2026-01-15 12:57:15 +0100

    trying to fix zdotdir

--- modules/zsh/module.nix
+++ modules/zsh/module.nix
@@ -3,55 +3,141 @@
   lib,
   ...
 }:
-wlib.wrapModule (
-  {config, ...}: let
-    cfg = config.settings;
-  in {
-    options = {
-      settings = {
-        keyMap = lib.mkOption {
-          type = lib.types.enum [
-            "viins"
-            "emacs"
-            "vicmd"
-          ];
+wlib.wrapModule ({config, ...}: let
+  cfg = config.settings;
+in {
+  options = {
+    settings = {
+      keyMap = lib.mkOption {
+        type = lib.types.enum [
+          "viins"
+          "emacs"
+          "vicmd"
+        ];
+        description = ''
+          keymap for zsh, pick between emacs vi, and vicmd, defaults to emacs mode.
+        '';
+        default = "emacs";
+      };
+
+      autocd = lib.mkOption {
+        type = lib.types.bool;
+        description = ''
+          lets you navigate to a directory just by typing the name/path. defaults to false;
+        '';
+        default = false;
+      };
+
+      completion.enable = lib.mkOption {
+        type = lib.types.bool;
+        description = ''
+          enable completion, false by default.
+        '';
+        default = false;
+      };
+
+      shellAliases = lib.mkOption {
+        type = with lib.types; attrsOf str;
+        description = ''
+          aliases
+        '';
+        default = {};
+      };
+
+      history = {
+        saveNoDups = lib.mkOption {
+          type = lib.types.bool;
           description = ''
-            keymap for zsh, pick between emacs vi, and vicmd, defaults to emacs mode.
+            doesn't save a new line if you type in a command already in the history, defaults to false.
           '';
-          default = "emacs";
+          default = false;
         };
-        ".zshrc" = lib.mkOption {
-          type = wlib.types.file config.pkgs;
-          default = let
-            aliasStr = builtins.concatStringsSep "\n" (
-              lib.mapAttrsToList (k: v: "alias -- ${lib.escapeShellArg k}=${lib.escapeShellArg v}")
-              cfg.shellAliases
-            );
-          in {
-            path = builtins.concatStringsSep "\n" [
-              (
-                if cfg.keyMap == "vicmd"
-                then "bindkey -a"
-                else if cfg.keyMap == "viins"
-                then "bindkey -v"
-                else "bindkey -e"
-              )
-              aliasStr
-            ];
-          };
+        expireDuplicatesFirst = lib.mkOption {
+          type = lib.types.bool;
+          description = ''
+            expire duplicates first.
+          '';
+          default = false;
+        };
+        append = lib.mkOption {
+          type = lib.types.bool;
+          description = ''
+            history from new zsh sessions will be appended to the hist file instead of replacing the old session history, defaults to false.
+          '';
+          default = false;
+        };
+        findNoDups = lib.mkOption {
+          type = lib.types.bool;
+          description = ''
+            ignore duplicate history when doing a reverse search.
+          '';
+          default = false;
+        };
+        ignoreDups = lib.mkOption {
+          type = lib.types.bool;
+          description = ''
+            sets the --histignoredups flag. TODO: figure out difference with saveNoDups for better docs.
+          '';
+          default = false;
+        };
+        ignoreSpace = lib.mkOption {
+          description = ''
+            lets you omit a command from the history if you put a space before it; defaults to true;
+          '';
+          default = true;
+        };
+      };
+      ".zshrc" = lib.mkOption {
+        type = wlib.types.file config.pkgs;
+        default = let
+          aliasStr = builtins.concatStringsSep "\n" (
+            lib.mapAttrsToList (k: v: "alias -- ${lib.escapeShellArg k}=${lib.escapeShellArg v}")
+            cfg.shellAliases
+          );
+        in {
+          content = builtins.concatStringsSep "\n" [
+            (
+              if cfg.keyMap == "vicmd"
+              then "bindkey -a"
+              else if cfg.keyMap == "viins"
+              then "bindkey -v"
+              else "bindkey -e"
+            )
+            aliasStr
+          ];
         };
       };
     };
-
-    config = let
-      RC = config.settings.".zshrc";
-
-      zshConfigDir = config.pkgs.linkFarmFromDrvs "zsh-config-directory" [
-        RC
-      ];
-    in {
-      package = config.pkgs.zsh;
-      env.Z_DOT_DIR = "${zshConfigDir}";
+  };
+  config = let
+    zshConfigDir = config.pkgs.linkFarmFromDrvs "yazi-config-directory" [
+      cfg.".zshrc"
+    ];
+  in {
+    flagSeparator = "=";
+    flags = {
+      "--histsavenodups" = cfg.history.saveNoDups;
+      "--histexpiredupsfirst" = cfg.history.expireDuplicatesFirst;
+      "--histappend" = cfg.history.append;
+      "--histfindnodups" = cfg.history.findNoDups;
+      "--histignoredups" = cfg.history.ignoreDups;
+      "--histignorespace" = cfg.history.ignoreSpace;
+      "--autocd" = cfg.autocd;
+      "--autolist" = cfg.completion.enable;
+      "--automenu" = cfg.completion.enable;
     };
-  }
-)
+    env = {
+      Z_DOT_DIR = "${zshConfigDir}";
+    };
+
+    package = config.pkgs.zsh;
+
+    meta.maintainers = [
+      {
+        name = "mrid22";
+        github = config.meta.maintainers.name;
+        githubId = 82091823;
+      }
+    ];
+  };
+})
