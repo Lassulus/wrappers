633ba8fae25c54dfabfcd12c398f62f76e1c92e8
Random Dude <randomdude16671@proton.me>
2026-06-20 16:28:59 +0530

    modules/swaync: init

---
+++ modules/swaync/module.nix
@@ -0,0 +1,42 @@
+{
+  config,
+  lib,
+  wlib,
+  ...
+}:
+let
+  jsonFmt = config.pkgs.formats.json;
+in
+{
+  _class = "wrapper";
+  options = {
+    configOptions = lib.mkOption {
+      type = jsonFmt.type;
+      default = { };
+      description = ''
+        SwayNotificationCenter configuration file. 
+        See {manpage}`swaync(5)`
+      '';
+    };
+    configFile = lib.mkOption {
+      type = wlib.types.file config.pkgs;
+      default.path = jsonFmt.generate "swaync-config" config.configOptions;
+    };
+    "style.css" = lib.mkOption {
+      type = wlib.types.file config.pkgs;
+      default.content = "";
+    };
+  };
+
+  config.package = config.pkgs.swaynotificationcenter;
+
+  config.flags = {
+    "--config" = config.configFile.path;
+    "--style" = config."style.css".path;
+  };
+
+  config.meta.maintainers = [
+    lib.maintainers.randomdude
+  ];
+  config.meta.platforms = lib.platforms.linux;
+}
